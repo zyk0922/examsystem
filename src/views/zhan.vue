36b9de5e6654e68e86d807d22afd76151c858ad3
zhangyakai <zhangyakai@qq.com>
2021-09-02 19:00:41 +0800

    int

---
+++ src/views/zhan.vue
@@ -0,0 +1,23 @@
+<template>
+   <div>
+
+   </div>
+</template>
+<script>
+export default {
+   data() {
+      return {
+          aaa:'',
+      }
+   },
+   methods:{
+   },
+   created(){
+   },
+   computed:{
+   },
+}
+</script>
+<style lang="less" scoped>
+
+</style>
