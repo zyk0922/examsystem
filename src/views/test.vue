ae17893fe7809d2504a10357f6fa4b9a7e09fab8
zhangyakai <zhangyakai@qq.com>
2021-09-02 19:17:53 +0800

    init

---
+++ src/views/test.vue
@@ -0,0 +1,23 @@
+<template>
+   <div>
+
+   </div>
+</template>
+<script>
+export default {
+   data() {
+      return {
+          data:1111111,
+      }
+   },
+   methods:{
+   },
+   created(){
+   },
+   computed:{
+   },
+}
+</script>
+<style lang="less" scoped>
+
+</style>
